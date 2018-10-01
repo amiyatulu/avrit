3aa5907aa3d4bd45ed0766c3cb5f18bd642b16f5
Amiya Behera <amiyatulu@gmail.com>
2018-10-01 10:45:14 +0530

    Avrit

---
+++ avrit_app/profiles_api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path, path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name="hello-viewset")
+router.register('profile', views.UserProfileViewSet)
+router.register('login', views.LoginViewSet, base_name="login")
+urlpatterns = [
+	path('hello-view/', views.HelloApiView.as_view()),
+	path('', include(router.urls)),
+]
