bc4a7b6b7ace7237d6b2ded79f0e1bab869f8816
Amiya Behera <amiyatulu@gmail.com>
2018-10-01 11:27:31 +0530

    profile update

--- avrit_app/profiles_api/serializers.py
+++ avrit_app/profiles_api/serializers.py
@@ -23,3 +23,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
 		user.save()
 
 		return user
+	def update(self, instance, validated_data):
+		for attr, value in validated_data.items():
+			if attr == 'password':
+				instance.set_password(value)
+			else:
+				setattr(instance, attr, value)
+		instance.save()
+		return instance
